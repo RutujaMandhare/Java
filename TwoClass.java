e6e8460a318721433284fba2cb40fbe477b86780
RutujaMandhare <gandhrerutuja@gmail.com>
2017-07-15 18:02:35 -0500

    Add files via upload

---
+++ TwoClass.java
@@ -0,0 +1,31 @@
+
+public class TwoClass {
+
+	
+
+	TwoClass() {
+	    System.out.println("Constructor of Computer class.");
+	  }
+	 
+	  void computer_method() {
+	    System.out.println("Power gone! Shut down your PC soon...");
+	  }
+	 
+	  public static void main(String[] args) {
+		  TwoClass my = new TwoClass();
+	    Laptop your = new Laptop();
+	 
+	    my.computer_method();
+	    your.laptop_method();
+	  }
+	}
+	 
+	class Laptop {
+	  Laptop() {
+	    System.out.println("Constructor of Laptop class.");
+	  }
+	 
+	  void laptop_method() {
+	    System.out.println("99% Battery available.");
+	  }
+	} 
